f5f4e6edda27370f353ccc60ae5ea8acc6d0eeeb
Calvin Macintosh <calvinmac633@gmail.com>
2020-04-28 22:26:09 -0400

    better nav

--- client/src/components/BootstrapNav/index.js
+++ client/src/components/BootstrapNav/index.js
@@ -9,11 +9,10 @@ function BootstrapNav() {
                 <div className="p-4 collapsedContent">
                     {/* <h5 className="text-white h4">Collapsed content</h5>
                     <span className="text-muted">Toggleable via the navbar brand.</span> */}
-                    <div className="navbar-end">
                         <div className="navbar-item buttons">
                             <div className="field is-grouped">
                                 <p className="control">
-                                    <a className="bd-tw-button button is-link is-outlined" target="_blank" href="https://github.com/Calvinmac633">
+                                    <a className="bd-tw-button is-small button is-link is-outlined" target="_blank" href="https://github.com/Calvinmac633">
                                         <span className="icon">
                                             <i className="fab fa-github"></i>
                                         </span>
@@ -23,7 +22,7 @@ function BootstrapNav() {
                                     </a>
                                 </p>
                                 <p className="control">
-                                    <a className="button is-link is-outlined" target="_blank" href="https://www.linkedin.com/in/calvin-macintosh-b5025198/">
+                                    <a className="button is-small is-link is-outlined" target="_blank" href="https://www.linkedin.com/in/calvin-macintosh-b5025198/">
                                         <span className="icon">
                                             <i className="fab fa-linkedin"></i>
                                         </span>
@@ -31,7 +30,7 @@ function BootstrapNav() {
                                     </a>
                                 </p>
                                 <p className="control">
-                                    <a onClick={() => alert("Email Function Available Shortly")} className="button is-link is-outlined" href="">
+                                    <a onClick={() => alert("Email Function Available Shortly")} className="button is-small is-link is-outlined" href="">
                                         <span className="icon">
                                             <i className="fas fa-envelope-open-text"></i>
                                         </span>
@@ -40,7 +39,7 @@ function BootstrapNav() {
                                 </p>
                                 <p className="control">
                                     <a href={resume} target="_blank"
-                                        className="button is-link is-outlined">
+                                        className="button is-small is-link is-outlined">
                                         <span className="icon">
                                             <i className="fas fa-check-square"></i>
                                         </span>
@@ -49,10 +48,9 @@ function BootstrapNav() {
                                 </p>
                             </div>
                         </div>
-                    </div>
                 </div>
             </div>
-            <nav className="navbar">
+            <nav className="navbar fixed-top">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon">
                         =
